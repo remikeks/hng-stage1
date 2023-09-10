894ae9ecb31864171d50830c3e9eb9424fa5d217
remikeks <remikeks@gmail.com>
2023-09-10 22:54:46 +0100

    bug fixes

--- hng_task_1/settings.py
+++ hng_task_1/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,13 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!l77ut_k5u9!z%914s_n-8e$rn69q^!o)q+bjo8veq5&_s&le4'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+SECRET_KEY = "django-insecure-!l77ut_k5u9!z%914s_n-8e$rn69q^!o)q+bjo8veq5&_s&le4"
 
-ALLOWED_HOSTS = []
+DEBUG = False
+
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition

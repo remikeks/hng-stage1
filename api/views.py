53febbc5c9b95e8de6157b88bee2a2002882ced0
remikeks <remikeks@gmail.com>
2023-09-11 12:56:23 +0100

    new app with correct url

---
+++ api/views.py
@@ -0,0 +1,20 @@
+from django.http import JsonResponse
+from datetime import datetime
+
+def index(request):
+    slack_name = request.GET.get("slack_name")
+    track = request.GET.get("track")
+
+    data = {
+        "slack_name": slack_name,
+        "current_day": datetime.utcnow().strftime('%A'),
+        "utc_time": datetime.now().strftime('%Y-%m-%dT%H:%M:%SZ'),
+        "track": track,
+        "github_file_url": "https://github.com/remikeks/hng-stage1/blob/main/api/views.py",
+        "github_repo_url": "https://github.com/remikeks/hng-stage1",
+        "status_code": 200
+    }
+    
+
+    return JsonResponse(data)
+
